98e6c2364daedd503d09a705e675d9ada0f88342
Quetellen <quetellen.bertazo@gmail.com>
2023-09-16 11:22:21 -0700

    PrStDeletion, rStUpdate, PrStTransaction

--- src/main/java/com/simplilearn/simplilearn_jdbc/_07_PrStDelete.java
+++ src/main/java/com/simplilearn/simplilearn_jdbc/_07_PrStDelete.java
@@ -21,69 +21,32 @@ public class _07_PrStDelete {
 	        String dbUsername = "root";
 	        String dbPassword = "rootroot";
 
-	        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-
-	        User user = new User();
-
 	        /*
-	         * Read the data from the User.
+	         * Read the user ID from the User.
 	         */
+
+	        int input = 0;
+
 	        try(Scanner scan= new Scanner(System.in)){
 
 	            System.out.println("Enter the user ID: ");
-	            user.setIdUser(scan.nextInt());
-	            scan.nextLine();
-
-	            System.out.println("Enter the username: ");
-	            user.setUsername(scan.nextLine());
-
-	            System.out.println("Enter the password: ");
-	            user.setPassword(scan.nextLine());
-
-	            System.out.println("Enter the first name: ");
-	            user.setFirstName(scan.nextLine());
-
-	            System.out.println("Enter the last name: ");
-	            user.setLastName(scan.nextLine());
-
-	            System.out.println("Enter the date of birth (yyyy-MM-dd): ");
-	            user.setBirth(formatter.parse(scan.nextLine()));
-
-	            System.out.println("Enter the status: ");
-	            user.setStatus(scan.nextLine());
-
-	        } catch (ParseException ex) {
-	            System.err.println("Error while reading input: " + ex.getMessage());
-	            System.exit(-1);
+	            input = scan.nextInt();
 	        }
 
-	        System.out.println(user);
-
 	        /*
-	         * Create the Update SQL Statement
+	         * Create the Delete SQL Statement
 	         */
-	        String sql = "UPDATE USER SET USERNAME = ?, PASSWORD = ?, FIRST_NAME = ?, LAST_NAME = ?, BIRTH = ?, STATUS = ? "
-	                   + "WHERE ID_USER = ?";
+	        String sql = "DELETE FROM USER WHERE ID_USER = ?";
 
 	        System.out.println("Query: " + sql);
 
 	        /*
-	         * Insert the data into the DB table;
+	         * Delete data from the DB table;
 	         */
 	        try(Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
 	            PreparedStatement prSt = con.prepareStatement(sql)) {
 
-	            prSt.setString(1, user.getUsername());
-	            prSt.setString(2, user.getPassword());
-	            prSt.setString(3, user.getFirstName());
-	            prSt.setString(4, user.getLastName());
-	            /*
-	             * We need to convert from java.util.Date into java.sql.Date.
-	             *  - user.getBirth() returns a java.util.Date
-	             */
-	            prSt.setDate(5, new Date(user.getBirth().getTime()));
-	            prSt.setString(6, user.getStatus());
-	            prSt.setInt(7, user.getIdUser());
+	            prSt.setInt(1, input);
 
 	            int rows = prSt.executeUpdate();
 
@@ -91,7 +54,7 @@ public class _07_PrStDelete {
 	                System.out.println("Number of rows affected: " + rows);
 	            }
 	            else {
-	                System.out.println("User not found, user ID: " + user.getIdUser());
+	                System.out.println("User not found, user ID: " + input);
 	            }
 
 	        } catch (SQLException ex) {
